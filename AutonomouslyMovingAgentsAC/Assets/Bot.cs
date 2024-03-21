75ff9a5bb655bb74f60395f396b5126a009bba3b
AngelCCO <116102695+AngelCCO@users.noreply.github.com>
2024-03-21 11:40:28 -0700

    Done
    A

--- AutonomouslyMovingAgentsAC/Assets/Bot.cs
+++ AutonomouslyMovingAgentsAC/Assets/Bot.cs
@@ -132,10 +132,46 @@ public class Bot : MonoBehaviour
         return false;
     }
 
+    bool TargetCanSeeMe()
+    {
+        Vector3 toAgent = this.transform.position - target.transform.position;
+        float lookingAngle = Vector3.Angle(transform.position, toAgent);
+
+        if(lookingAngle < 60)
+            return true;
+        return false;
+    }
+
+    bool coolDown = false;
+    void BehaviourCoolDown()
+    {
+        coolDown = false;
+    }
+
+    bool TargetInRange()
+    {
+        if(Vector3.Distance(this.transform.position, target.transform.position) < 10)
+            return true;
+        return false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (CanSeeTarget())
-        CleverHide();
+        if (!coolDown)
+        {
+            if(!TargetInRange())
+            {
+                Wander();
+            }
+            if (CanSeeTarget() && TargetCanSeeMe())
+            {
+                CleverHide();
+                coolDown = true;
+                Invoke("BehaviourCoolDown", 5);
+            }
+            else
+                Pursue();
+        }
     }
 }
